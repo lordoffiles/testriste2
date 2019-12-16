9b83f3ec5174b6b927fac31d4c0d47e2a041a5b8
lordoffiles <vincentroy444@me.com>
2019-12-16 15:58:08 -0500

    fixed some stuff

--- main.py
+++ main.py
@@ -35,27 +35,18 @@ def draw_menu(stdscr):
         if stdscr.getch() == ord("q"):
             break
 
-
         bottom_line = "Key "+str(key)
 
         # collisions
 
         # draw stuff loop ##########
+
+        stdscr.addstr(1, 0, str(stdscr.getyx()))
+        stdscr.addstr(1, stdscr.getyx()[1], str(stdscr.getyx()))
+
+        # stdscr.addstr(0, 0, tiles(t))
+
         # bound the cursor position
-
-    #draw stuff loop
-
-    stdscr.addstr(0, 0, str(stdscr.getyx()))
-    stdscr.addstr(0, stdscr.getyx()[1], str(stdscr.getyx()))
-
-    #stdscr.addstr(0, 0, tiles(t))
-
-    # Refresh the screen
-
-    stdscr.refresh()
-
-    while(True):
-        #bound the cursor poisition
         cursor_x = max(0, cursor_x)
         cursor_x = min(screen_width - 1, cursor_x)
 
@@ -67,30 +58,11 @@ def draw_menu(stdscr):
         stdscr.addstr(0, 0, "Time {}".format(round(time_end - time_start)))
 
         # debug zone
-        if bottom_line != "-1":
+        if bottom_line != "Key -1":
             stdscr.addstr(screen_height - 1, 0, "".join([" " for i in range(screen_width - 1)]))
             stdscr.addstr(screen_height - 1, 0, "{}".format(bottom_line))
 
-        # Refresh the screen
         stdscr.refresh()
-
-
-        #calculate elapsed time and draw it
-        #time_end = time.time()
-        #stdscr.addstr(0, 0, "Width: {0} Height: {1}".format(screen_width, screen_height))
-
-
-    #     if k == curses.KEY_DOWN:
-    #         cursor_y = cursor_y + 1
-    #     elif k == curses.KEY_UP:
-    #         cursor_y = cursor_y - 1
-    #     elif k == curses.KEY_RIGHT:
-    #         cursor_x = cursor_x + 1
-    #     elif k == curses.KEY_LEFT:
-    #         cursor_x = cursor_x - 1
-
-    # # Wait for next input
-    # k = stdscr.getch()
 
 
 def main():
