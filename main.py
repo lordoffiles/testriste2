2ec7ec9dcc05a0640dc2f250b6ac4856c596788a
Alain Benoit <alain.benoit@ocpm.qc.ca>
2019-12-16 13:45:51 -0500

    test01

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sys,os
 import curses
 import time
@@ -34,7 +33,7 @@ def draw_menu(stdscr):
 
 
         # Refresh the screen
-        
+
         stdscr.refresh()
 
 
@@ -117,24 +116,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-# # quantity = 3
-# # itemno = 567
-# # price = [20, 30]
-# # myorder = "I want to pay {2[0]} dollars for {0} pieces of item {1}."
-# # print(myorder.format(quantity, itemno, price))
-#
-# a = 2
-# b = 5
-#
-# if a > b: print("a is greater than b")
-#
-# i = 1
-# while i > 6:
-#   print(i)
-#   i += 1
-
-f = open("test", "rt")
-
-print(f)
->>>>>>> test
